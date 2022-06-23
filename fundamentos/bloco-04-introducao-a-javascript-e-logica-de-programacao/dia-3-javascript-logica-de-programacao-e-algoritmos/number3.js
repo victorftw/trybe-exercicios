4db087acdc73f1e1c179290f5af9b54b7a2d05c8
Victor Hugo <victor.hiraiftw@outlook.com>
2022-06-23 20:14:31 -0300

    *ESTUDAR MAIS TARDE*

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/number3.js
@@ -0,0 +1,19 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = array[0];
+let menor = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > maior.length) {
+    maior = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < menor.length) {
+    menor = array[index];
+  }
+}
+
+console.log('A menor palavra é:', menor);
+console.log('A maior palavra é:', maior);
