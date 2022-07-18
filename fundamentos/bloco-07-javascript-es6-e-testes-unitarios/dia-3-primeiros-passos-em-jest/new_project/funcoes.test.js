31e00296a23fe13ee9c9d9a39be12da01b568bc7
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-18 20:49:24 -0300

    exercicio 2 - teste 3 concluido

--- fundamentos/bloco-07-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/new_project/funcoes.test.js
+++ fundamentos/bloco-07-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/new_project/funcoes.test.js
@@ -1,4 +1,4 @@
-const { sum, myRemove } = require('./sum');
+const { sum, myRemove } = require('./funcoes');
 
 describe('Testes da função sum', () => {
   test('Verifica se sum(4, 5) retorna 9', () => {
@@ -21,5 +21,9 @@ describe('Testes da função myRemove', () => {
 
   test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
