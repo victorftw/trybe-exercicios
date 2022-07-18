3f4ce07e9cd8ae8f34426ad0dee27f28a08cd3b3
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-18 18:35:22 -0300

    exercicio 1 - teste 3 concluido

--- fundamentos/bloco-07-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/new_project/sum.test.js
+++ fundamentos/bloco-07-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/new_project/sum.test.js
@@ -8,4 +8,8 @@ describe('Testes da função sum', () => {
   test('Verifica se sum(0, 0) retorna 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
+
+  test(`Verifica se sum(4, '5') retorna erro`, () => {
+    expect(() => sum(4, '5')).toThrow('parameters must be numbers');
+  });
 });
