1fc58d5603fbafd00922b3ca17fcf5efacadc98d
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-22 12:38:19 -0300

    exercicio para fixar 2.2 e 2.3

--- fundamentos/bloco-08-introducao-hofs/dia-2-hofs-forEach-find-some-every-sort/paraFixar2.js
+++ fundamentos/bloco-08-introducao-hofs/dia-2-hofs-forEach-find-some-every-sort/paraFixar2.js
@@ -2,4 +2,23 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = numbers.find((number) => number % 15 === 0);
 
-console.log(findDivisibleBy3And5);
+// console.log(findDivisibleBy3And5);
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = names.find((name) => name.length === 5);
+
+// console.log(findNameWithFiveLetters);
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+function findMusic(id) {
+  const musica = musicas.find((item) => item.id === id);
+  return musica;
+}
+
+console.log(findMusic('31031685'));
