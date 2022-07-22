dd9284a495256d23e366162a544c4a2c117a19c9
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-22 14:46:47 -0300

    exercicio para fixar 3.2

--- fundamentos/bloco-08-introducao-hofs/dia-2-hofs-forEach-find-some-every-sort/paraFixar3.js
+++ fundamentos/bloco-08-introducao-hofs/dia-2-hofs-forEach-find-some-every-sort/paraFixar3.js
@@ -3,4 +3,17 @@ const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 const hasName = (arr, name) =>
   arr.some((nome) => (nome === name ? true : false));
 
-console.log(hasName(names, 'Ana'));
+// console.log(hasName(names, 'Ana'));
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) =>
+  arr.every((item) => (item.age >= minimumAge ? true : false));
+
+console.log(verifyAges(people, 18));
