68847e3b2d49201681b78a46d0058a300e1e1035
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-22 09:50:16 -0300

    exercicio 3 refeito

--- fundamentos/bloco-08-introducao-hofs/dia-1-hofs/exercise3.js
+++ fundamentos/bloco-08-introducao-hofs/dia-1-hofs/exercise3.js
@@ -2,19 +2,22 @@ const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const correcao = (gabarito, respostas) => {
-  let pontuacao = 0;
-  for (let index in respostas) {
-    if (respostas[index] === gabarito[index]) {
-      pontuacao += 1;
-    } else if (respostas[index] === 'N.A') {
-      pontuacao += 0;
-    } else {
-      pontuacao -= 0.5;
-    }
+  if (respostas === gabarito) {
+    return 1;
   }
-  return pontuacao;
+  if (respostas === 'N.A') {
+    return 0;
+  }
+  return -0.5;
 };
 
-const resultado = (gabarito, respostas, func) => func(gabarito, respostas);
+const resultado = (gabarito, respostas, callback) => {
+  let pontos = 0;
+  for (let index in gabarito) {
+    const callbackReturn = callback(gabarito[index], respostas[index]);
+    pontos += callbackReturn;
+  }
+  return `Sua nota foi de ${pontos} pontos.`;
+};
 
 console.log(resultado(RIGHT_ANSWERS, STUDENT_ANSWERS, correcao));
