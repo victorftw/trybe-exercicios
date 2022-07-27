4ee9fb7e3052b799df68d56c23490d8893851e96
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-27 14:50:05 -0300

    exercicio fixação 3.2

--- fundamentos/bloco-08-introducao-hofs/dia-5-spread-operator-parametro-rest-destructuring/paraFixar3.js
+++ fundamentos/bloco-08-introducao-hofs/dia-5-spread-operator-parametro-rest-destructuring/paraFixar3.js
@@ -1,7 +1,17 @@
 const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
-saudacoes[1](saudacoes[0]); // Olá
+// saudacoes[1](saudacoes[0]); // Olá
 
 const [ola, funcao] = saudacoes;
 
-funcao(ola);
+// funcao(ola);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida);
