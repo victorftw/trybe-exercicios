96c51a4c1915f56547e30ae5fd026c7d90d940d8
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-27 12:18:37 -0300

    exercicio fixação 2

---
+++ fundamentos/bloco-08-introducao-hofs/dia-5-spread-operator-parametro-rest-destructuring/paraFixar2.js
@@ -0,0 +1,13 @@
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const trappistEnterprise = { ...user, ...jobInfos };
