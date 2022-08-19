4acbbf0bacc7d643831e89eaf9b5a407f76471f3
Victor Hugo <victor.hiraiftw@outlook.com>
2022-08-19 19:27:38 -0300

    exercicios de fixação concluidos

--- front-end/bloco-11-estado-eventos-forms-com-react/dia-1-componente-com-estado-e-eventos/exercises/src/Counter.jsx
+++ front-end/bloco-11-estado-eventos-forms-com-react/dia-1-componente-com-estado-e-eventos/exercises/src/Counter.jsx
@@ -5,15 +5,24 @@ class Counter extends Component {
     count: 0,
   };
 
+  getButtonColor = (num) => (num % 2 === 0 ? 'green' : 'white');
+
   handleClick = () => {
-    this.setState((num) => ({
-      count: num.count + 1,
-    }));
+    this.setState(
+      ({ count }) => ({
+        count: count + 1,
+      }),
+      () => console.log(this.getButtonColor(this.state.count))
+    );
   };
 
   render() {
     return (
-      <button type="text" onClick={this.handleClick}>
+      <button
+        type="text"
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.getButtonColor(this.state.count) }}
+      >
         {this.state.count}
       </button>
     );
