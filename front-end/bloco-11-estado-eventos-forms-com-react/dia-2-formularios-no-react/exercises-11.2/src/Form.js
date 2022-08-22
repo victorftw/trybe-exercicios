169e7bef8b3b86cf61ede21e98c75eb2ba8480d4
Victor Hugo <victor.hiraiftw@outlook.com>
2022-08-22 16:08:46 -0300

    exercicio 2 fixação concluido

--- front-end/bloco-11-estado-eventos-forms-com-react/dia-2-formularios-no-react/exercises-11.2/src/Form.js
+++ front-end/bloco-11-estado-eventos-forms-com-react/dia-2-formularios-no-react/exercises-11.2/src/Form.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Form extends Component {
   state = {
     about: '',
+    language: '',
+    recebeEmail: false,
+    suaFoto: '',
   };
 
-  textareaChange = (event) => {
-    const { value } = event.target;
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      about: value,
+      [name]: value,
     });
   };
   render() {
@@ -17,15 +21,26 @@ class Form extends Component {
         <h1>Forms</h1>
         <form>
           <label>
-            <select name="select">
-              <option>Javascript</option>
-              <option>Python</option>
+            <select name="language" onChange={this.handleChange}>
+              <option value="javascript">Javascript</option>
+              <option value="python">Python</option>
             </select>
           </label>
           <label>
             Nos conte algo sobre você! :D
             <br />
-            <textarea name="about" onChange={this.textareaChange}></textarea>
+            <textarea name="about" onChange={this.handleChange}></textarea>
+          </label>
+          <label>
+            Deseja receber nossos emails?
+            <input
+              type="checkbox"
+              name="recebeEmail"
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <label>
+            <input type="file" name="suaFoto" onChange={this.handleChange} ></input>
           </label>
         </form>
       </>
